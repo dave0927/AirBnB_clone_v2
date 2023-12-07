b6b73648bd6aa7b74d27c48cbc671c1af3ccc72e
Dawit Tadesse <dawittadesse927@gmail.com>
2023-12-07 17:42:09 +0300

    creates and distributes an archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,33 +4,29 @@ Fabric script that distributes an archive to your web servers
 """
 
 from fabric.api import put, run, env
-from os import path
+from os.path import exists
 
-env.hosts = ["54.84.14.126", "54.164.201.219"]
-env.user = "ubuntu"
-
+env.hosts = ["3.90.80.220", ""]
 
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
+    """Distributes an archive"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
 
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo mkdir -p {}{}/".format(path, no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, no_ext))
+        run("sudo rm /tmp/{}".format(file_name))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext))
+        run("sudo rm -rf {}{}/web_static".format(path, no_ext))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+        run("ln -s {}{}/ /data/web_static/current".format(path, no_ext))
 
-        print("New version deployed!")
+        print("*** New version deployed! ***")
         return True
-
-    return False
+    except:
+        return False
